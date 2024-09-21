1345e4f51ce525a3f44ccfc36f918ba0d3cc5922
Darie-Nistor Nicolae <darie.nistornicolae@coolblue.nl>
2024-09-22 00:08:32 +0200

    Update .gitIgnore

--- Farmazia/Services/Seller/SellerServiceProtocol.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol SellerServiceProtocol {
-  func createSeller(_ seller: SellerModel) async throws
-  func getSeller(id: String) async throws -> SellerModel?
-  func updateSeller(_ seller: SellerModel) async throws
-  func deleteSeller(id: String) async throws
-}
