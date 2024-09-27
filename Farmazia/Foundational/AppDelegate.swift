0ea97c9cbda8e695dd929b55b16381f7dfb1b758
Darie-Nistor Nicolae <darie.nistornicolae@coolblue.nl>
2024-09-27 20:02:11 +0200

    Remove FirebaseMessaging

--- Farmazia/Foundational/AppDelegate.swift
+++ Farmazia/Foundational/AppDelegate.swift
@@ -1,32 +1,13 @@
 import Foundation
 import UIKit
 import FirebaseCore
-import FirebaseMessaging
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
     FirebaseApp.configure()
-    Messaging.messaging().delegate = self
-    application.registerForRemoteNotifications()
 
     return true
-  }
-
-  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    Messaging.messaging().apnsToken = deviceToken
-  }
-
-  func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    print("Failed to register for remote notifications: \(error)")
-  }
-}
-
-// MARK: - MessagingDelegate
-
-extension AppDelegate: MessagingDelegate {
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-    print("Firebase registration token: \(String(describing: fcmToken))")
   }
 }
